a0407220319db240836fb776ed739c2c15e246f6
WilliamAbon <96871965+WilliamAbon@users.noreply.github.com>
2025-12-06 14:05:56 +0700

    Create indo.lua

---
+++ fol/indo.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WilliamAbon/UniversalSR/refs/heads/main/fol/woodrus%201.txt"))()--1
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WilliamAbon/UniversalSR/refs/heads/main/fol/woodsrus2.txt"))()--1
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WilliamAbon/UniversalSR/refs/heads/main/fol/woodsrus%203.txt"))()--1
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WilliamAbon/UniversalSR/refs/heads/main/fol/woodsrus%204.txt"))()--1
